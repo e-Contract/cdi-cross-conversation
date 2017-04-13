b495e092a363aa874e688c3a27247fa6968ddb33
Frank Cornelis <info@e-contract.be>
2017-04-13 16:59:01 +0200

    apache license

--- cdi-cross-conversation-tests/src/test/java/test/integ/be/e_contract/cdi/crossconversation/CrossConversationScopedAndroidCodeServlet.java
+++ cdi-cross-conversation-tests/src/test/java/test/integ/be/e_contract/cdi/crossconversation/CrossConversationScopedAndroidCodeServlet.java
@@ -1,8 +1,19 @@
 /*
- * eID Android project.
+ * CDI Cross Conversation project.
  *
- * Copyright 2016-2017 e-Contract.be BVBA. All rights reserved.
- * e-Contract.be BVBA proprietary/confidential. Use is subject to license terms.
+ * Copyright 2016-2017 e-Contract.be BVBA.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
  */
 package test.integ.be.e_contract.cdi.crossconversation;
 
--- cdi-cross-conversation-tests/src/test/java/test/integ/be/e_contract/cdi/crossconversation/CrossConversationScopedTest.java
+++ cdi-cross-conversation-tests/src/test/java/test/integ/be/e_contract/cdi/crossconversation/CrossConversationScopedTest.java
@@ -1,8 +1,19 @@
 /*
- * eID Android project.
+ * CDI Cross Conversation project.
  *
- * Copyright 2016-2017 e-Contract.be BVBA. All rights reserved.
- * e-Contract.be BVBA proprietary/confidential. Use is subject to license terms.
+ * Copyright 2016-2017 e-Contract.be BVBA.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
  */
 package test.integ.be.e_contract.cdi.crossconversation;
 
@@ -51,7 +62,7 @@ public class CrossConversationScopedTest {
                         CrossConversationScopedInvalidateSessionServlet.class, CrossConversationScopedAndroidCodeServlet.class)
                 .addPackages(true, CrossConversationScoped.class.getPackage())
                 .addAsWebInfResource(CrossConversationScopedTest.class
-                                .getResource("/META-INF/beans.xml"),
+                        .getResource("/META-INF/beans.xml"),
                         "beans.xml")
                 .addAsManifestResource(CrossConversationScopedTest.class.getResource("/META-INF/services/javax.enterprise.inject.spi.Extension"),
                         "services/javax.enterprise.inject.spi.Extension");
